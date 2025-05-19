f4c14552529fd5d0e5b817d10f51b033caf03fa3
Boris Prochnau <boris.prochnau@colenio.com>
2025-05-19 16:49:24 +0200

    feat: accept input and display list of inputs

--- vendor_app/src/App.tsx
+++ vendor_app/src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '../public/vite.svg'
+import {TextField, Button} from '@mui/material'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <h1>Vendor Training</h1>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <MyFormComponent>
+            </MyFormComponent>
+        </>
+    )
+}
+
+function MyFormComponent() {
+    const [inputValue, setInputValue] = useState("");
+    const [items, setItems] = useState<string[]>([]);
+
+    const handleSubmit = (event:any) => {
+        event.preventDefault(); // verhindert Seitenreload
+        console.log("Form submitted with:", inputValue);
+
+        setItems([...items, inputValue])
+        setInputValue("")
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    id="standard-basic"
+                    label="Standard"
+                    variant="standard"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+                <Button type="submit">Absenden</Button>
+            </form>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default App
