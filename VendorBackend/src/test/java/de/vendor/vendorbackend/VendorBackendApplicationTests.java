afb628c308912d0cf669e3754f0b614c5a65cee2
Boris Prochnau <boris.prochnau@colenio.com>
2025-05-21 20:04:53 +0200

    feat: add bootstrapped backend

---
+++ VendorBackend/src/test/java/de/vendor/vendorbackend/VendorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.vendor.vendorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
